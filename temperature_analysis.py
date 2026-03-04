ca2032a163074b3694b026e9b75692feb2d43e9b
priyadevmpd-eng <priyadevmpd@gmail.com>
2026-03-04 16:48:46 +0530

    Add files via upload

---
+++ temperature_analysis.py
@@ -0,0 +1,3 @@
+print("Title: The Alchemist | Author: Paulo Coelho | Price: $12.99")
+print("Title: 1984 | Author: George Orwell | Price: $9.99")
+print("Title: Atomic Habits | Author: James Clear | Price: $14.99")
